88669d89f6d4b016a2f42014e8ea9994eaca24dc
WenpingWang <wenping0820@163.com>
2016-01-31 15:22:28 +0800

    Add ‘AndroidCodeGenerator’ plugin

---
+++ src/AndroidCodeGenerator.java
@@ -0,0 +1,13 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by wenping on 2016/1/31.
+ */
+public class AndroidCodeGenerator extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+
+    }
+}
